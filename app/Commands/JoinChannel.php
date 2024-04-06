a5c161b14bd9de64bbfde9c8d9e7aa0051bf9443
Cawe541 <cawe.wewe04@gmail.com>
2024-04-06 14:45:14 -0300

    GG-1: Joining the user voice channel.

--- app/Commands/JoinChannel.php
+++ app/Commands/JoinChannel.php
@@ -37,21 +37,28 @@ class JoinChannel extends Command
     /**
      * Handle the command.
      *
-     * @param  \Discord\Parts\Channel\Message  $message
-     * @param  array  $args
-     * @return void
+     * @param \Discord\Parts\Channel\Message $message
+     * @param array $args
+     * @return \React\Promise\ExtendedPromiseInterface
      */
     public function handle($message, $args)
     {
+        $voiceChannel = $message->member->getVoiceChannel();
 
-        $channel = $this->discord()->getChannel("channel-id");
+        if (is_null($voiceChannel)) {
+            return $this
+                ->message()
+                ->title(sprintf('%s, por favor, entre em algum canal de voz e repita o comando.', $message->member->display_name))
+                ->content('Você deve estar em algum canal de voz para me convidar!.')
+                ->send($message);
+        }
 
-        $this->discord()->joinVoiceChannel($channel);
-        
+        $this->discord()->joinVoiceChannel($voiceChannel);
+
         return $this
             ->message()
-            ->title('JoinChannel')
-            ->content('Hello world!')
+            ->title(sprintf('%s me convidou para o canal de voz "%s"!.', $message->member->display_name, $voiceChannel->name))
+            ->content(sprintf('Entrei no canal de voz "%s"!', $voiceChannel->name))
             ->send($message);
     }
 }
